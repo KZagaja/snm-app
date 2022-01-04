bd588792cf5940d02a00d7fe37c4c0219fbc7195
KZagaja <kordian.zagaja@gmail.com>
2022-01-04 03:21:14 +0100

    2 files modified

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const userSchema = new mongoose.Schema({
+  // Basic info
+  idx: { type: Number, unique: true },
+  nickname: { type: String, required: true },
+  email: { type: String },
+  password: { type: String, required: true },
+  recoverPasswordHash: { type: String },
+  avatar: { type: String },
+  registeredAt: { type: Date },
+  isActive: { type: Boolean, default: false },
+  timezone: { type: String, default: 'Europe/Warsaw' },
+  accountLevel: { type: Number, default: 0 },
+  //Level - 0 - user
+  //Level - 1 - player
+  //Level - 1.5 - chimu
+  //Level - 2 - mod
+  //Level - 3 - game master
+  //Level - 4 - engineers
+  //Level - 5 - H@
+
+  // Additional info
+  profileFields: {
+    kp: { type: String },
+    settlement: { type: String },
+    age: { type: Number },
+    height: { type: Number },
+    weight: { type: Number },
+    otherInfo: { type: String },
+  },
+});
